a5dfb48c19054fcb129518cfc3264b42222e41d9
Josue Gimbernard <josue@minibiota.com>
2025-06-18 08:47:44 -0400

    Update species-data.js

--- species-data.js
+++ species-data.js
@@ -4,7 +4,7 @@ const speciesData = [
     commonName: "Marsh Periwinkle",
     scientificName: "Littoraria irrorata",
     realm: "Saltwater",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Littoraria_irrorata_01.JPG/640px-Littoraria_irrorata_01.JPG",
+    imageUrl: "https://images.squarespace-cdn.com/content/6851878b48103e0785d2a2a0/f5b11e43-6927-4333-85d8-6001c69ec03a/Mangrove+Forest+Biome+9+Jun+2025.jpg?content-type=image%2Fjpeg",
     conservationStatus: "Least Concern",
     trophicRole: "Grazer"
   }
